edbc04a194b50bb57bb17f5970afca81f27f0e4c
Daniel Bogorad <danielbog.7@gmail.com>
2016-03-27 20:42:07 +0300

    improved generate_nmea_sentence()

--- RPiGPSDemo/rpi_gps_demo.c
+++ RPiGPSDemo/rpi_gps_demo.c
@@ -9,66 +9,40 @@
 
 char* generate_nmea_sentence(void){
 	srand(time(NULL));
+	
 	char* nmea = malloc(100 * sizeof(char)); // allocate string of 100
 	memset(nmea, '\0', sizeof(nmea));
+	
 	int r = rand() % 2;
 	if(r == 0) strcat(nmea, "$GPGGA,");
 	else strcat(nmea, "$GPRMC,");
 	
-	// 2. append time (first convert to a string) NOTE: is there a nicer way to do this ?
-	char str[30];
+	char str[100];
 	sprintf(str, "%d,", rand());
-	strcat(nmea, str);
 	
-	//continue to generate the correct type of nmea sentence
 	if(strncmp(nmea, "$GPGGA", 6) == 0){
-		// 3. latitude can only be between 0 and 90
-		memset(str, '\0', sizeof(str));
-		sprintf(str, "%f,N,", ((double)(rand() / ((double)((unsigned)RAND_MAX + 1))*0.9)) * 100);
-		strcat(nmea, str);
-		// 4. longitude can only be between 0 and 180
-		memset(str, '\0', sizeof(str));
-		sprintf(str, "%f,E,", ((double)(rand() / ((double)((unsigned)RAND_MAX + 1))*0.9)) * 200);
-		strcat(nmea, str);
-		// 5. fix quality
-		memset(str, '\0', sizeof(str));
-		sprintf(str, "%d,", (uint8_t)((rand()%8)+1)); // fix can be from 0-8 but 0 is invalid signal. dont bother right now
-		strcat(nmea, str);
-		// 6. num of tracked satellites
-		memset(str, '\0', sizeof(str));
-		sprintf(str, "%d,", (uint8_t)((rand()%9)+2)); // arbitrary number but no too big (24 sats in space at all)
-		strcat(nmea, str);
+		// 3. latitude can only be between 0 and 90, longitude between 0 and 180
+		sprintf(str + strlen(str), "%f,N,", ((double)(rand() / ((double)((unsigned)RAND_MAX + 1))*0.9)) * 100);
+		sprintf(str + strlen(str), "%f,E,", ((double)(rand() / ((double)((unsigned)RAND_MAX + 1))*0.9)) * 200);
+		// 5. fix quality, num of tracked satellites
+		sprintf(str + strlen(str), "%d,", (uint8_t)((rand()%8)+1)); // fix can be from 0-8 but 0 is invalid signal. dont bother right now
+		sprintf(str + strlen(str), "%d,", (uint8_t)((rand()%9)+2));
 		// 7. HDOP (for example between 0 and 30, (the more the worse!))
-		memset(str, '\0', sizeof(str));
-		sprintf(str, "%f,", ((double)(rand() / ((double)((unsigned)RAND_MAX + 1))*0.3)) * 100); // arbitrary number but no too big (24 sats in space at all)
-		strcat(nmea, str);
+		sprintf(str + strlen(str), "%f,", ((double)(rand() / ((double)((unsigned)RAND_MAX + 1))*0.3)) * 100); // arbitrary number but no too big (24 sats in space at all)
 		// 8. altitude above sea level
-		memset(str, '\0', sizeof(str));
-		sprintf(str, "%f,M,", ((double)(rand() / ((double)((unsigned)RAND_MAX + 1)))) * 1000); // arbitrary number but no too big (24 sats in space at all)
-		strcat(nmea, str);
-		// 9. height above gedid, time since last DGPS update (sec), DGPS satation id, checksum
-		memset(str, '\0', sizeof(str));
-		sprintf(str, "%f,M,0,0,E*68", ((((double)(rand() / ((double)((unsigned)RAND_MAX + 1))*0.191)) * 1000) - 106)); // arbitrary number but no too big (24 sats in space at all)
+		sprintf(str + strlen(str), "%f,M,", ((double)(rand() / ((double)((unsigned)RAND_MAX + 1)))) * 1000); // arbitrary number but no too big (24 sats in space at all)
+		// 9. height above geoid(-106 to 85), time since last DGPS update (sec), DGPS satation id, checksum
+		sprintf(str + strlen(str), "%f,M,0,0,E*68", ((((double)(rand() / ((double)((unsigned)RAND_MAX + 1))*0.191)) * 1000) - 106)); // arbitrary number but no too big (24 sats in space at all)
 		strcat(nmea, str);
 	} else {
-		strcat(nmea, "A,"); //status
-		// 4. latitude can only be between 0 and 90
-		memset(str, '\0', sizeof(str));
-		sprintf(str, "%f,N,", ((double)(rand() / ((double)((unsigned)RAND_MAX + 1))*0.9)) * 100);
-		strcat(nmea, str);
-		// 5. longitude can only be between 0 and 180
-		memset(str, '\0', sizeof(str));
-		sprintf(str, "%f,E,", ((double)(rand() / ((double)((unsigned)RAND_MAX + 1))*0.9)) * 200);
-		strcat(nmea, str);
-		//speed over the ground IN KNOTS
-		memset(str, '\0', sizeof(str));
-		sprintf(str, "%f,", ((double)(rand() / ((double)((unsigned)RAND_MAX + 1))*0.55)) * 100);
-		strcat(nmea, str);
-		// track angle in degrees True (course, thus 0-360), date, magnetic variation, and checksum, all not relevant for us
-		memset(str, '\0', sizeof(str));
-		sprintf(str, "%f,230394,3.1,W*68", ((double)(rand() / ((double)((unsigned)RAND_MAX + 1))*0.36)) * 1000);
+		sprintf(str + strlen(str), "A, %f,N,", ((double)(rand() / ((double)((unsigned)RAND_MAX + 1))*0.9)) * 100);
+		sprintf(str + strlen(str), "%f,E,", ((double)(rand() / ((double)((unsigned)RAND_MAX + 1))*0.9)) * 200);
+		sprintf(str + strlen(str), "%f,", ((double)(rand() / ((double)((unsigned)RAND_MAX + 1))*0.55)) * 100);
+		sprintf(str + strlen(str), "%f,230394,3.1,W*68", ((double)(rand() / ((double)((unsigned)RAND_MAX + 1))*0.36)) * 1000);
 		strcat(nmea, str);
 	}
+	
+	printf("%s\n", nmea);
 	return nmea;
 }
 
