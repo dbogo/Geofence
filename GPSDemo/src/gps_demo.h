8079e8d04a34af4514c529714d1e8ca45394a3ee
Daniel Bogorad <danielbog.7@gmail.com>
2016-04-03 22:09:13 +0300

    created first manual build scripts

--- GPSDemo/src/gps_demo.h
+++
@@ -1,7 +0,0 @@
-#ifndef GPS_DEMO_H
-#define GPS_DEMO_H
-
-#include "../GPSInterface.h"
-
-#endif /* GPS_DEMO_H */
-
---
+++ build_debug.sh
@@ -0,0 +1,17 @@
+
+#compile libraries with position independant code
+gcc -c -fpic RPiGPSDemo/src/*.c -o RPiGPSDemo/build/rpi_gps_demo.o
+gcc -c -fpic GPSDemo/src/*.c -o GPSDemo/build/gps_demo.o
+
+
+#create shared libraries from object file
+#RPiGPSDemo
+gcc -shared -o RPiGPSDemo/dist/Debug/GNU-Linux/libRPiGPSDemo.so RPiGPSDemo/build/rpi_gps_demo.o
+
+#GPSDemo
+gcc -shared -o GPSDemo/dist/Debug/GNU-Linux/libGPSDemo.so GPSDemo/build/gps_demo.o
+
+
+gcc -o dist/Debug/GNU-Linux/geofence src/*.c -LRPiGPSDemo/dist/Debug/GNU-Linux -LGPSDemo/dist/Debug/GNU-Linux -Llog4c/lib -Ilog4c/include -Wl,-rpath=/home/bogo/Desktop/Geofence/RPiGPSDemo/dist/Debug/GNU-Linux -Wl,-rpath=/home/bogo/Desktop/Geofence/log4c/lib -Wall -lRPiGPSDemo -dynamic -lGPSDemo -dynamic -llog4c -dynamic
+
+rm main.*
