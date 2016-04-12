da1c4281a5bf82febe7cbe6b0a8d4e29f0543a64
Daniel Bogorad <danielbog.7@gmail.com>
2016-04-12 20:54:09 +0300

    added utils.h file with some stuff

---
+++ src/utils.h
@@ -0,0 +1,12 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+
+#define PI 3.14159265
+#define TIME_TO_WAIT_SEC 1
+
+
+#endif /* UTILS_H */
