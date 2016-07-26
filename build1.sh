a27e5c173ad7a3a33f3bad553c3d432249c7bd78
Daniel Bogorad <danielbog.7@gmail.com>
2016-07-26 19:43:07 +0300

    cleaened unnecessary stuff in build1.sh build script

--- build1.sh
+++ build1.sh
@@ -15,7 +15,7 @@ gcc -shared -o RPiGPSDemo/dist/Debug/GNU-Linux/libRPiGPSDemo.so RPiGPSDemo/build
 gcc -shared -o GPSDemo/dist/Debug/GNU-Linux/libGPSDemo.so GPSDemo/build/gps_demo.o
 
 
-gcc -o dist/Debug/GNU-Linux/geofence src/*.c src/serial/serialInterface.c src/pifaceCAD/cad_utils.c -LRPiGPSDemo/dist/Debug/GNU-Linux -Llog4c/lib -Ilog4c/include -Lpifacecad/libpifacecad/ -Lpifacecad/libmcp23s17/ -Ipifacecad/libpifacecad/src/ -Wl,-rpath=./RPiGPSDemo/dist/Debug/GNU-Linux -Wl,-rpath=./log4c/lib -Wall -lRPiGPSDemo -dynamic -llog4c -dynamic -lm -lpifacecad -lmcp23s17
+gcc -o dist/Debug/GNU-Linux/geofence src/*.c src/serial/serialInterface.c src/pifaceCAD/cad_utils.c -LRPiGPSDemo/dist/Debug/GNU-Linux -Llog4c/lib -Ilog4c/include -Lpifacecad/libpifacecad/ -Lpifacecad/libmcp23s17/ -Ipifacecad/libpifacecad/src/ -Wl,-rpath=./RPiGPSDemo/dist/Debug/GNU-Linux -Wl,-rpath=./log4c/lib -Wall -lRPiGPSDemo -llog4c -lm -lpifacecad -lmcp23s17
 
 #gcc -o dist/Debug/GNU-Linux/geofence src/*.c src/serial/serialInterface.c src/pifaceCAD/cad_utils.c -LGPSDemo/dist/Debug/GNU-Linux -Llog4c/lib -Ilog4c/include -Lpifacecad/libpifacecad/ -Lpifacecad/libmcp23s17/ -Ipifacecad/libpifacecad/src/ -Wl,-rpath=./GPSDemo/dist/Debug/GNU-Linux -Wl,-rpath=./log4c/lib -Wall -lGPSDemo -dynamic -llog4c -dynamic -lm -lpifacecad -lmcp23s17
 
@@ -23,8 +23,3 @@ gcc -o dist/Debug/GNU-Linux/geofence src/*.c src/serial/serialInterface.c src/pi
 
 #gcc -o dist/Debug/GNU-Linux/geofence src/*.c -LRPiGPSDemo/dist/Debug/GNU-Linux -LGPSDemo/dist/Debug/GNU-Linux -Llog4c/lib -Ilog4c/include -Wl,-rpath=/home/bogo/Desktop/Geofence/RPiGPSDemo/dist/Debug/GNU-Linux -Wl,-rpath=/home/bogo/Desktop/Geofence/log4c/lib -Wall -lRPiGPSDemo -dynamic -lGPSDemo -dynamic -llog4c -dynamic -lm
 
-rm cad_utils.*
-rm serialInterface.*
-rm main.*
-rm GPSInterface.*
-rm logInterface.*
