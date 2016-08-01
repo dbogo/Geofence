50813057df491e769e5ebb652ea831b796fc1206
Daniel Bogorad <danielbog.7@gmail.com>
2016-08-01 18:00:21 +0300

    added a simple wiringPi led interface for dubugging

---
+++ src/led.c
@@ -0,0 +1,21 @@
+
+#include "led.h"
+
+int init_wiringPi(){
+	return wiringPiSetup();
+}
+
+int set_pin(int p){
+	pinMode(p, OUTPUT);
+	return 0;
+}
+
+int light_led(int p){
+	digitalWrite(p, HIGH);
+	reutrn 0;
+}
+
+int led_off(int p){
+	digitalWrite(p, LOW);
+	return 0;
+}
---
+++ src/led.h
@@ -0,0 +1,13 @@
+#ifndef LED_H
+#define LED_H
+
+#include "../wiringPi/wiringPi/wiringPi.h"
+
+int init_wiringPi();
+int set_pin(int p);
+
+int light_led(int p);
+int led_off(int p);
+
+
+#endif /* LED_H */	
