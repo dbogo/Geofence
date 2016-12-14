62800c4a13a503d7cff992b5ffb1e01631e5ae5d
Daniel Bogorad <danielbog.7@gmail.com>
2016-12-14 14:31:33 +0200

    added mavlink's c example code with a few edits. not ready yet.

---
+++ src/c_uart_interface_example/setpoint_helpers.c
@@ -0,0 +1,76 @@
+#include "setpoint_helpers.h"
+#include <stdio.h>
+
+// choose 1 of the top 3
+
+void set_position(float x, float y, float z, mavlink_set_position_target_local_ned_t &sp){
+	sp.type_mask =
+		MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_POSITION;
+
+	sp.coordinate_frame = MAV_FRAME_LOCAL_NED;
+
+	sp.x   = x;
+	sp.y   = y;
+	sp.z   = z;
+
+	printf("POSITION SETPOINT XYZ = [ %.4f , %.4f , %.4f ] \n", sp.x, sp.y, sp.z);
+}
+
+
+void set_velocity(float vx, float vy, float vz, mavlink_set_position_target_local_ned_t &sp){
+	sp.type_mask =
+		MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_VELOCITY     ;
+
+	sp.coordinate_frame = MAV_FRAME_LOCAL_NED;
+
+	sp.vx  = vx;
+	sp.vy  = vy;
+	sp.vz  = vz;
+
+	//printf("VELOCITY SETPOINT UVW = [ %.4f , %.4f , %.4f ] \n", sp.vx, sp.vy, sp.vz);
+
+}
+
+
+void
+set_acceleration(float ax, float ay, float az, mavlink_set_position_target_local_ned_t &sp)
+{
+
+	// NOT IMPLEMENTED
+	fprintf(stderr,"set_acceleration doesn't work yet \n");
+	throw 1;
+
+
+	sp.type_mask =
+		MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_ACCELERATION &
+		MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_VELOCITY     ;
+
+	sp.coordinate_frame = MAV_FRAME_LOCAL_NED;
+
+	sp.afx  = ax;
+	sp.afy  = ay;
+	sp.afz  = az;
+}
+
+// the next two need to be called after one of the above
+
+void
+set_yaw(float yaw, mavlink_set_position_target_local_ned_t &sp)
+{
+	sp.type_mask &=
+		MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_YAW_ANGLE ;
+
+	sp.yaw  = yaw;
+
+	printf("POSITION SETPOINT YAW = %.4f \n", sp.yaw);
+
+}
+
+void
+set_yaw_rate(float yaw_rate, mavlink_set_position_target_local_ned_t &sp)
+{
+	sp.type_mask &=
+		MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_YAW_RATE ;
+
+	sp.yaw_rate  = yaw_rate;
+}
---
+++ src/c_uart_interface_example/setpoint_helpers.h
@@ -0,0 +1,93 @@
+#ifndef SETPOINT_HELPERS
+#define SETPOINT_HELPERS
+
+#include "mavlink/include/mavlink/v1.0/common/mavlink.h"
+
+
+/**
+ * Defines for mavlink_set_position_target_local_ned_t.type_mask
+ *
+ * Bitmask to indicate which dimensions should be ignored by the vehicle
+ *
+ * a value of 0b0000000000000000 or 0b0000001000000000 indicates that none of
+ * the setpoint dimensions should be ignored.
+ *
+ * If bit 10 is set the floats afx afy afz should be interpreted as force
+ * instead of acceleration.
+ *
+ * Mapping:
+ * bit 1: x,
+ * bit 2: y,
+ * bit 3: z,
+ * bit 4: vx,
+ * bit 5: vy,
+ * bit 6: vz,
+ * bit 7: ax,
+ * bit 8: ay,
+ * bit 9: az,
+ * bit 10: is force setpoint,
+ * bit 11: yaw,
+ * bit 12: yaw rate
+ * remaining bits unused
+ *
+ * Combine bitmasks with bitwise &
+ *
+ * Example for position and yaw angle:
+ * uint16_t type_mask =
+ *     MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_POSITION &
+ *     MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_YAW_ANGLE;
+ */
+
+                                                // bit number  876543210987654321
+#define MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_POSITION     0b0000110111111000
+#define MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_VELOCITY     0b0000110111000111
+#define MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_ACCELERATION 0b0000110000111111
+#define MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_FORCE        0b0000111000111111
+#define MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_YAW_ANGLE    0b0000100111111111
+#define MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_YAW_RATE     0b0000010111111111
+
+
+
+/*
+ * Set target local ned position
+ *
+ * Modifies a mavlink_set_position_target_local_ned_t struct with target XYZ locations
+ * in the Local NED frame, in meters.
+ */
+void set_position(float x, float y, float z, mavlink_set_position_target_local_ned_t &sp);
+
+/*
+ * Set target local ned velocity
+ *
+ * Modifies a mavlink_set_position_target_local_ned_t struct with target VX VY VZ
+ * velocities in the Local NED frame, in meters per second.
+ */
+void set_velocity(float vx, float vy, float vz, mavlink_set_position_target_local_ned_t &sp);
+
+/*
+ * Set target local ned acceleration
+ *
+ * Modifies a mavlink_set_position_target_local_ned_t struct with target AX AY AZ
+ * accelerations in the Local NED frame, in meters per second squared.
+ */
+void set_acceleration(float ax, float ay, float az, mavlink_set_position_target_local_ned_t &sp);
+
+// the next two need to be called after one of the above
+
+/*
+ * Set target local ned yaw
+ *
+ * Modifies a mavlink_set_position_target_local_ned_t struct with a target yaw
+ * in the Local NED frame, in radians.
+ */
+void set_yaw(float yaw, mavlink_set_position_target_local_ned_t &sp);
+
+/*
+ * Set target local ned yaw rate
+ *
+ * Modifies a mavlink_set_position_target_local_ned_t struct with a target yaw rate
+ * in the Local NED frame, in radians per second.
+ */
+void set_yaw_rate(float yaw_rate, mavlink_set_position_target_local_ned_t &sp);
+
+#endif /* SETPOINT_HELPERS */
